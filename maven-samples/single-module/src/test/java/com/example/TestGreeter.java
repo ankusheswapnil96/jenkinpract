0b52359a34b3c6affad8d6782b6e19f697146bf7
ankusheswapnil96 <124359056+ankusheswapnil96@users.noreply.github.com>
2023-12-28 21:20:20 +0530

    Update TestGreeter.java with new method

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.greaterThan;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 import static org.junit.matchers.JUnitMatchers.containsString;
 
 public class TestGreeter {
@@ -23,6 +23,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
+  @Test
+  public void custMethod() {
+    assertTrue(true);
+  }
+
 
   @Test
   public void greetShouldIncludeGreetingPhrase() {
